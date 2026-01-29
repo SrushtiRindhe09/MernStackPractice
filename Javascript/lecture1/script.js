74acc32576d28847dd86ec6b45cb60da1072e24a
SrushtiRindhe09 <rindhesrushti2006@gmail.com>
2026-01-29 20:33:38 +0530

    write basic code

---
+++ Javascript/lecture1/script.js
@@ -0,0 +1,24 @@
+console.log("hello Javascript");
+
+// control + ` it will open your terminal
+
+// let const var
+// var old tarike, don't use it.
+
+let num = 10.5;
+console.log(num);
+
+let ar = "Rohit";
+ar = "mohit";
+console.log(ar)
+
+const ids = 20;
+console.log(ids);
+// ids = 30; values can't be changed
+
+var x = 10;
+x=20;
+console.log(x);
+
+alert("hello from js file");
+prompt("enter your name");
